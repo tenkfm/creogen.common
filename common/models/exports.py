fe5c4158fbd656949511907959dadd050cfb5ba5
Tony <anton.rogachevskyi@gmail.com>
2025-08-06 13:32:58 +0200

    0.3.5

---
+++ common/models/exports.py
@@ -0,0 +1,23 @@
+from enum import Enum
+from datetime import datetime
+from typing import List, Optional
+from pydantic import BaseModel, Field
+from common.services.firebase.firebase_object import FirebaseObject
+
+class TTExport(FirebaseObject):
+    campaign_name: str
+    ad_groups_count: int
+    pixel_id: str
+    pixel_event: str
+    locations: List[str]
+    languages: List[str]
+    budget: float
+    bid: float
+    identity_id: str
+    text: str
+    url: str
+    event_name: str
+    
+    @staticmethod
+    def collection_name():
+        return "tt_exports"
