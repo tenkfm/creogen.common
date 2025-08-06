e068bea71d381d337066fecc038f229f281dac39
Tony <anton.rogachevskyi@gmail.com>
2025-08-06 02:28:35 +0200

    openai

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="creogen-common",
-    version="0.3.0",
+    version="0.3.1",
     packages=find_packages(),  # найдёт папку common/common
     install_requires=[         # List of dependencies
         'firebase-admin',
-        'pydantic'
+        'pydantic',
+        'openai'
     ],
 )
