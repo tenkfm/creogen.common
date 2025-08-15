6630e7d5f1ed9c933500f0f4fabdf8fa8c7d264d
Tony <anton.rogachevskyi@gmail.com>
2025-08-15 17:18:10 +0200

    profiles

---
+++ common/models/profile.py
@@ -0,0 +1,14 @@
+from enum import Enum
+from datetime import datetime
+from typing import List, Optional, Dict
+from pydantic import BaseModel, Field
+from common.services.firebase.firebase_object import FirebaseObject
+
+class Profile(FirebaseObject):
+    id: Optional[str] = None
+    email: str
+    heygen_api_key: str
+    
+    @staticmethod
+    def collection_name():
+        return "profiles"
